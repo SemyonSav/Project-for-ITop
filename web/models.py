4e493f77898eed97b9e187d2fe02b8f6e072549a
Semen Savelev <ctvbxm@mail.ru>
2023-12-11 22:35:24 +0300

    Создание базовых моделей

--- web/models.py
+++ web/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+class NewsTag(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+
+class News(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    post_date = models.DateField(verbose_name='Дата публикации', default=timezone.now)
+    tags = models.ManyToManyField(NewsTag, verbose_name='Теги', blank=True)
+    text = models.TextField(verbose_name='Текст новости')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    text = models.TextField(verbose_name='Текст комментария')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
