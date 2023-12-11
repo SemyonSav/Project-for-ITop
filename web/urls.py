2b936a5279e7521ca658dab2b00bc58368741e7c
Semen Savelev <ctvbxm@mail.ru>
2023-12-11 22:13:58 +0300

    Добавление базового шаблона

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from web.views import main_view
+from web.views import *
 
 urlpatterns = [
     path("", main_view, name="main"),
+    path("registration/", registration_view, name="registration"),
+    path("auth/", auth_view, name="auth"),
+    path("saved/", saved_view, name="saved"),
+    path("news/add/", news_add_view, name="news_add")
 ]
--- web/views.py
+++ web/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def main_view(request):
     return render(request, "web/main.html")
 
+
+def registration_view(request):
+    pass
+
+
+def auth_view(request):
+    pass
+
+
+def saved_view(request):
+    pass
+
+
+def news_add_view(request):
+    pass
