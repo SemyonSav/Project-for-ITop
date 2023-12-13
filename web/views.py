f1e73ecab8cebef4f3f0a46da625f8c3444ef070
Semen Savelev <ctvbxm@mail.ru>
2023-12-13 21:45:37 +0300

    Регистрация и авторизация

--- web/views.py
+++ web/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, get_user_model, authenticate, login
+from django.shortcuts import render, redirect
+
+from web.forms import *
+
+User = get_user_model()
 
 
 def main_view(request):
@@ -6,11 +11,40 @@ def main_view(request):
 
 
 def registration_view(request):
-    pass
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            is_success = True
+    return render(request, "web/registration.html", {
+        "form": form, "is_success": is_success
+    })
 
 
 def auth_view(request):
-    pass
+    form = AuthForm()
+    if request.method == 'POST':
+        form = AuthForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is None:
+                form.add_error(None, "Введены неверные данные")
+            else:
+                login(request, user)
+                return redirect("main")
+    return render(request, "web/auth.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("main")
 
 
 def saved_view(request):
